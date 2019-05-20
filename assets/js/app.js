80e1124b1d4f56bc8acdb55ff6b9f6b4e66c0c78
Alan Vardy <alanvardy@gmail.com>
2019-05-19 21:29:18 -0600

    navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import 'bootstrap';
-import css from "../css/app.scss"
+import css from "../css/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -10,9 +9,10 @@ import css from "../css/app.scss"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+import "./navbar";

e473a4feb5442a37bd950ebc40156ddffcb47554
Alan Vardy <alanvardy@gmail.com>
2020-01-02 15:31:05 -0800

    lets try this

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,30 +1,20 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 module.exports = (env, options) => ({
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
   entry: {
-    './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
   },
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, '../priv/static/js')
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -33,17 +23,13 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.s?css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '../css/app.css'
-    }),
-    new CopyWebpackPlugin([{
-      from: 'static/',
-      to: '../'
-    }])
+    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new MinifyPlugin(),
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });

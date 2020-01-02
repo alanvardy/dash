0c559fd11eb8a88bf64df19e9e545a3988a6fbd0
Alan Vardy <alanvardy@gmail.com>
2020-01-02 09:48:29 -0800

    extend session length

--- lib/dash_web/endpoint.ex
+++ lib/dash_web/endpoint.ex
@@ -41,6 +41,7 @@ defmodule DashWeb.Endpoint do
   # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
+    max_age: 24*60*60*30,       # 30 days
     key: "_dash_key",
     signing_salt: "mMAuD+lz"
 

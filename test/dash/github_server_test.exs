6250d570da030af0edfba24e2b58d2312e5865bb
Alan Vardy <alanvardy@gmail.com>
2020-01-24 17:11:01 -0800

    Test genservers

---
+++ test/dash/github_server_test.exs
@@ -0,0 +1,27 @@
+defmodule Dash.GithubServerTest do
+  @moduledoc false
+  alias Dash.Api.Github.Server
+  use Dash.DataCase, async: true
+  import Dash.Factory
+
+  describe "init" do
+    test "starts with issues: nil" do
+      user = insert(:user)
+      assert Server.init(%{id: user.id}) == {:ok, %{issues: nil}}
+    end
+  end
+
+  describe "handle_call" do
+    test "responds with issues" do
+      assert Server.handle_call(:issues, "from", %{issues: "issues"}) ==
+               {:reply, "issues", %{issues: "issues"}}
+    end
+  end
+
+  describe "handle_cast" do
+    test "updates issues" do
+      assert Server.handle_cast({:update_issues, "other issues"}, %{issues: "issues"}) ==
+               {:noreply, %{issues: "other issues"}}
+    end
+  end
+end

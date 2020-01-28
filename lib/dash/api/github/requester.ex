c7ad2b2913e4ebb4e0d696f241039d3a38cb1611
Alan Vardy <alanvardy@gmail.com>
2020-01-27 19:53:00 -0800

    Error handling and catch strange messages

--- lib/dash/api/github/requester.ex
+++ lib/dash/api/github/requester.ex
@@ -19,11 +19,17 @@ defmodule Dash.Api.Github.Requester do
       {:ok, issues} ->
         GenServer.cast(parent, {:update_issues, issues})
 
-        # {:error, message} ->
-        #   Logger.error("Harvest server error in #{__MODULE__}: #{inspect(message)}")
+      {:error, message} ->
+        Logger.error("Harvest server error in #{__MODULE__}: #{inspect(message)}")
     end
 
     Process.send_after(self(), :tick, @refresh_time)
+    {:noreply, state}
+  end
+
+  @doc "For catching unknown messages"
+  def handle_info(message, state) do
+    Logger.error("Received unknown message in #{__MODULE__} \nMESSAGE: #{inspect(message)}\n STATE: #{inspect(state)}")
     {:noreply, state}
   end
 end

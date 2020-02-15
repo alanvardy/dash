2cbb9d0270468f017a48c2b07dcdf1279bda4e08
Alan Vardy <alanvardy@gmail.com>
2020-02-15 09:52:42 -0800

    Render 403

--- lib/dash_web/views/error_view.ex
+++ lib/dash_web/views/error_view.ex
@@ -13,4 +13,8 @@ defmodule DashWeb.ErrorView do
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
+
+  def render("403.html", _assigns) do
+    "Forbidden"
+  end
 end

d77f1b19e25c0b0e9493f67b57a182b9a204f047
Alan Vardy <alanvardy@gmail.com>
2020-04-19 12:09:29 -0700

    Adjust text as white or black depending on label color

--- lib/dash_web/views/page_view.ex
+++ lib/dash_web/views/page_view.ex
@@ -26,6 +26,7 @@ defmodule DashWeb.PageView do
         "#{div(result, 86_400)} days"
     end
   end
+
   def weekdays_left(%{time: %{weekdays_left: weekdays_left}}) do
     weekdays_left
   end
@@ -37,4 +38,21 @@ defmodule DashWeb.PageView do
   end
 
   def month(_), do: "current month"
+
+  def maybe_white_text(hex_color) do
+    ~r/.{2}/
+    |> Regex.scan(hex_color)
+    |> List.flatten()
+    |> Enum.map(&hex_to_int/1)
+    |> Enum.reduce(fn x, acc -> x + acc end)
+    |> case do
+      num when num < 450 -> "has-text-white"
+      _ -> ""
+    end
+  end
+
+  defp hex_to_int(hex) do
+    <<number>> = Base.decode16!(hex, case: :mixed)
+    number
+  end
 end
